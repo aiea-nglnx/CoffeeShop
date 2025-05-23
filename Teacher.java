a3bd3da691664805a4543daffd49149206527b93
Angelina <167708709+aiea-nglnx@users.noreply.github.com>
2025-05-22 23:12:37 -1000

    Made Teacher class self-validating

--- Teacher.java
+++ Teacher.java
@@ -1,12 +1,9 @@
-import java.util.Scanner;
-
 /**
  * An interactive program Java program that models
  * an environment by creating classes for 2 related objects.
  * 
  * @author     Pao, Angelina
- * @assignment ICS 111 Assignment 10 - Object 1
- * @date       04/13/2025
+ * @thing      Object 1
  * @bugs       Short description of bugs in the program, if any.
  */
  
@@ -19,10 +16,10 @@ import java.util.Scanner;
    
    // Constructor that allows you to create the Teacher object
    public Teacher (String name, String academy, int staffID, boolean teachMixed) {
-      this.name = name;
-      this.academy = academy;
-      this.teachMixed = teachMixed;      
-      this.staffID = staffID;
+      this.setName(name);
+      this.setAcademy(academy);
+      this.setTeachMixed(teachMixed);      
+      this.setStaffID(staffID);
    }
    
    // toString method
@@ -50,16 +47,36 @@ import java.util.Scanner;
    }
    
    // Adding Mutator methods to Teacher class that allows modification
-   public void setName(String newName) {
+   public void setName(String newName) throws SchoolException {
+    if (newName.length() >= 2) {
       this.name = newName;
+    }
+    else {      
+      throw new SchoolException("ERROR! Teacher's name must be at least 2 characters long");
+    }
    }
-   public void setAcademy(String newAcademy) {
-      this.academy = newAcademy;
+   public void setAcademy(String newAcademy) throws SchoolException {
+    if (newAcademy.equals("Academy of Arts & Communication") || newAcademy.equals("Academy of Health & Sciences") || newAcademy.equals("Academy of Industrial & Engineering Technology") || newAcademy.equals("Academy of Natural Resources") || newAcademy.equals("Academy of Professional & Public Services")){  
+     this.academy = newAcademy;
+    }
+    else {
+     throw new SchoolException("ERROR! Sorry but " +  newAcademy + " must be 1 of the following and an applicable option");
+    }
    }
-   public void setStaffID(int newStaffID) {
-      this.staffID = newStaffID;
+   public void setStaffID(int newStaffID) throws SchoolException{
+    if (newStaffID >= 10000 && newStaffID <= 99999) {  
+     this.staffID = newStaffID;
+    }
+    else {
+     throw new SchoolException("ERROR! Sorry but " + staffID + " must be a proper 5-digit long number.");
+    }
    }
-   public void setTeachMixed(boolean newTeachMixed) {
-      this.teachMixed = newTeachMixed;
+   public void setTeachMixed(boolean newTeachMixed) throws SchoolException{
+    if (newTeachMixed.equals("true") || newTeachMixed.equals("false")) {  
+     this.teachMixed = newTeachMixed;
+    }
+    else {
+     throw new SchoolException("ERROR! Sorry but " + newTeachMixed + " must be either true or false.");
+    }
    }
 }
