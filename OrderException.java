83430fac9c5704f2c046746000c1734f0e63613d
Angelina <167708709+aiea-nglnx@users.noreply.github.com>
2025-05-23 21:24:37 -1000

    Create OrderException.java

---
+++ OrderException.java
@@ -0,0 +1,9 @@
+/**
+ * A custom exception class for order objects
+ */
+
+public class OrderException extends Exception {
+    public OrderException(String message) {
+        super(message);
+    }
+}
