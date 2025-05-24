67b8d8fbbf1e6202e53dfa715494e3882b7679db
Angelina <167708709+aiea-nglnx@users.noreply.github.com>
2025-05-23 21:24:06 -1000

    Create CustomerException.java

---
+++ CustomerException.java
@@ -0,0 +1,9 @@
+/**
+ * A custom exception class for Customer objects
+ */
+
+public class CustomerException extends Exception {
+    public CustomerException(String message) {
+        super(message);
+    }
+}
